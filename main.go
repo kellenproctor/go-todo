fbf45f2ce507d3b06b0f1035962f565474c4f666
Kellen Proctor <kellenproctor@gmail.com>
2022-10-27 14:06:13 -0700

    Add fiber v2 and set up basic server

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func main() {
-    fmt.Println("App running")
+	app := fiber.New()
+	app.Use(cors.New())
+
+	api := app.Group("/api")
+
+	//Test handler
+	api.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("App running")
+	})
+
+	log.Fatal(app.Listen(":5000"))
 }
